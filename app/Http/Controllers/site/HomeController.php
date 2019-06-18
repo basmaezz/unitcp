aa85e1dc599d8683ec2e3d4667d717ec24f2b521
basmaezz <basma.aboelezz89@gmail.com>
2019-06-18 15:10:13 +0200

    Upload Exam Error

--- app/Http/Controllers/site/HomeController.php
+++ app/Http/Controllers/site/HomeController.php
@@ -179,9 +179,9 @@ class HomeController extends Controller
         $view = view('public.fac-exam', $data)->render();
         return response()->json(['status' => true, 'item' => $view]);
 
-//        $exams = ['exams' => Exam::where('faculty_id', $id)->paginate(3)];
-//        $exams_v = view('public.exam-result', $exams)->render();
-//        $exams_v_pag_v = view('public.paginate', $exams)->render();
+        $exams = ['exams' => Exam::where('faculty_id', $id)->get()];
+        $exams_v = view('public.exam-result', $exams)->render();
+        $exams_v_pag_v = view('public.paginate', $exams)->render();
 //        return response()->json(['status' => true, 'item' => $view, 'exams' => $exams_v, 'paginate' => $exams_v_pag_v]);
         return response()->json(['status' => true, 'item' => $view, 'exams' => $exams_v, 'paginate' => $exams_v_pag_v]);
     }
--- resources/views/public/view-exam.blade.php
+++ resources/views/public/view-exam.blade.php
@@ -53,8 +53,9 @@
 
         <div class="navbar-search">
 
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+            {{--<form class="form-inline my-2 my-lg-0">--}}
+                <form method="get" class="form-inline my-2 my-lg-0" action="{{route('panel.exam.search-s')}}">
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" name="txtsearch">
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
