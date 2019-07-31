4bd7335ede5bf124033d7e2e7a5b24d619cfe216
basmaezz <basma.aboelezz89@gmail.com>
2019-07-31 15:00:20 -0700

    Updated Paginate

--- resources/views/public/search.blade.php
+++ resources/views/public/search.blade.php
@@ -104,12 +104,21 @@
                             </li>
                         @endforeach
 
-
-
-
-
                 </ul>
                 <div id="pag_view">
+                    <nav class="pagination-wrapper" >
+                        <ul class="pagination justify-content-end ">
+                            <li class="page-item disabled">
+                                <a class="page-link" href="#" tabindex="-1">Previous</a>
+                            </li>
+                            <li class="page-item"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item">
+                                <a class="page-link" href="#">Next</a>
+                            </li>
+                        </ul>
+                    </nav>
 
                 </div>
 
